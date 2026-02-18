e17b46bad2914ab4b1179191744ffec6d837d965
Greg Reinacker <gregr@rassoc.com>
2026-02-18 15:05:02 -0700

    slightly lighten comment color

--- init.lua
+++ init.lua
@@ -806,6 +806,7 @@ require('lazy').setup({
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       vim.cmd.colorscheme 'tokyonight-night'
+      vim.api.nvim_set_hl(0, 'Comment', { fg = '#646e9f', italic = false })
     end,
   },
 
